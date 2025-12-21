4b787b902d3b85deae20bef9dbc48c337792fe10
winebarrel <sugawara@winebarrel.jp>
2025-12-22 00:45:57 +0900

    Fix for deprecated

--- VerticalBattery/VerticalBatteryApp.swift
+++ VerticalBattery/VerticalBatteryApp.swift
@@ -17,7 +17,8 @@ struct VerticalBatteryApp: App {
                 Text("N/A")
             }
             Divider()
-            Button("About \(Bundle.main.infoDictionary![kCFBundleNameKey as String]!)") {
+            let bundleName = Bundle.main.infoDictionary![kCFBundleNameKey as String] as! String
+            Button("About \(bundleName)") {
                 let githubRepo = "github.com/winebarrel/VerticalBattery"
                 NSApp.orderFrontStandardAboutPanel(options: [
                     NSApplication.AboutPanelOptionKey.applicationIcon: NSImage(named: "AppIcon")!,
